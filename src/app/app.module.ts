74e3b5c48a2879431dbb2fe8e4721a5595aa256c
Evgueni Tsygankov <info@rusmex.com>
2019-08-21 20:23:57 -0500

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector, InjectionToken } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {createCustomElement } from '@angular/elements';
+import { ActivatedRouteSnapshot } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,21 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+    provide: new InjectionToken('externalUrlRedirectResolver'),
+    useValue: (route: ActivatedRouteSnapshot) => {
+    }
+  }
+],
+  entryComponents: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+  constructor(private injector: Injector) {
+  }
+
+  ngDoBootstrap(){
+    customElements.define('my-app', createCustomElement(AppComponent, {injector: this.injector}));
+  }
+}
